557d8e892bfaa8ae2d0594ad4c725e1aec539a05
chrisrycx <chrisrycx@gmail.com>
2019-05-15 17:54:40 -0600

    Final presentation prep bokeh

--- Bokeh101/bokeh101.py
+++ Bokeh101/bokeh101.py
@@ -20,17 +20,17 @@ y3 = -1/6*x + 1
 #plt.show()
 
 #Basic plot using Bokeh
-#output_file("basics.html") #Output to static HTML file
-#p = figure(title="Basic Example", x_axis_label='x', y_axis_label='y')
-#p.line(x, y, line_width=2) # add a line renderer
-#show(p) # show the results
+output_file("basics.html") #Output to static HTML file
+p = figure(title="Basic Example", x_axis_label='x', y_axis_label='y')
+p.line(x, y, line_width=5) # add a line renderer
+show(p) # show the results
 
 #Layout and share axis
-output_file("layout.html") #Output to static HTML file
-p = figure(title="Layout Example", x_axis_label='x', y_axis_label='y')
-p.line(x, y, color="blue", line_width=2) # add a line renderer
-p.line(x, y2, color="red", line_width=2)
-p2 = figure(title="Layout Example 2", x_axis_label='x', y_axis_label='y',
-            x_range=p.x_range)
-p2.line(x, y3, color="red", line_width=2)
-show(row(p,p2)) # show the results
+#output_file("layout.html") #Output to static HTML file
+#p = figure(title="Layout Example", x_axis_label='x', y_axis_label='y')
+#p.line(x, y, color="blue", line_width=2) # add a line renderer
+#p.line(x, y2, color="red", line_width=2)
+#p2 = figure(title="Layout Example 2", x_axis_label='x', y_axis_label='y',
+#            x_range=p.x_range)
+#p2.line(x, y3, color="red", line_width=2)
+#show(row(p,p2)) # show the results
